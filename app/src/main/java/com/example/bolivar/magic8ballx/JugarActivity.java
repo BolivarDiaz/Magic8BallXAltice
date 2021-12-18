387be40a39d5e62950928800df34e4270aeee1a0
Bolivar Diaz <bolivarrafael95@gmail.com>
2021-12-18 15:54:32 -0400

    Add Speech to Text Fuction

--- app/src/main/java/com/example/bolivar/magic8ballx/JugarActivity.java
+++ app/src/main/java/com/example/bolivar/magic8ballx/JugarActivity.java
@@ -1,6 +1,8 @@
 package com.example.bolivar.magic8ballx;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
 import android.hardware.Sensor;
@@ -9,8 +11,10 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Surface;
 import android.view.View;
 import android.view.WindowManager;
@@ -18,11 +22,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 
 public class JugarActivity extends AppCompatActivity implements SensorEventListener,View.OnClickListener {
 
 // variables
-
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
     float valores[]=new float[3];
     int vz;
     boolean respondiendo;
@@ -75,20 +82,9 @@ public class JugarActivity extends AppCompatActivity implements SensorEventListe
         }else{
             // sin sensor
             Toast.makeText(this, R.string.toast_sinsensor, Toast.LENGTH_SHORT).show();
-           botonnosensor=findViewById(R.id.boton_nosensor);
-
+            botonnosensor=findViewById(R.id.boton_nosensor);
             botonnosensor.setOnClickListener(this);
-
-
-
-
         }
-
-
-
-
-
-
 
     }
 
@@ -96,79 +92,8 @@ public class JugarActivity extends AppCompatActivity implements SensorEventListe
     @Override
     public void onClick(View view) {
 
-        if(respondiendo==false) {
-            resultado.setText("");
-            respondiendo = true;
+        voiceToText();
 
-            if (MainActivity.sonido) {
-                mp.start();
-            }
-
-            new CountDownTimer(tiempo_respuesta, 1000) {
-
-
-                @Override
-                public void onTick(long l) {
-                    // do nothing
-                }
-
-                @Override
-                public void onFinish() {
-                    int numero = (int) (Math.random() * 14) + 1;
-
-                    switch (numero){
-
-
-                        case 1:
-                            resultado.setText(R.string.respuesta1);
-                            break;
-                        case 2:
-                            resultado.setText(R.string.respuesta2);
-                            break;
-                        case 3:
-                            resultado.setText(R.string.respuesta3);
-                            break;
-                        case 4:
-                            resultado.setText(R.string.respuesta4);
-                            break;
-                        case 5:
-                            resultado.setText(R.string.respuesta5);
-                            break;
-                        case 6:
-                            resultado.setText(R.string.respuesta6);
-                            break;
-                        case 7:
-                            resultado.setText(R.string.respuesta7);
-
-                            break;
-                        case 8:
-                            resultado.setText(R.string.respuesta8);
-                            break;
-                        case 9:
-                            resultado.setText(R.string.respuesta9);
-                            break;
-                        case 10:
-                            resultado.setText(R.string.respuesta10);
-                            break;
-                        case 11:
-                            resultado.setText(R.string.respuesta11);
-                            break;
-                        case 12:
-                            resultado.setText(R.string.respuesta12);
-                            break;
-                        case 13:
-                            resultado.setText(R.string.respuesta13);
-                            break;
-                        case 14:
-                            resultado.setText(R.string.respuesta14);
-                            break;
-
-                    }
-                    respondiendo = false;
-                }
-            }.start();
-
-        }
     }
 
 // reunudando y deteniendo sensores
@@ -241,59 +166,8 @@ public class JugarActivity extends AppCompatActivity implements SensorEventListe
                     @Override
                     public void onFinish() {
                        int numero = (int) (Math.random() * 14) + 1;
-
-                       switch (numero){
-
-
-                           case 1:
-                               resultado.setText(R.string.respuesta1);
-                               break;
-                           case 2:
-                               resultado.setText(R.string.respuesta2);
-                               break;
-                           case 3:
-                               resultado.setText(R.string.respuesta3);
-                               break;
-                           case 4:
-                               resultado.setText(R.string.respuesta4);
-                               break;
-                           case 5:
-                               resultado.setText(R.string.respuesta5);
-                               break;
-                           case 6:
-                               resultado.setText(R.string.respuesta6);
-                               break;
-                           case 7:
-                               resultado.setText(R.string.respuesta7);
-
-                               break;
-                           case 8:
-                               resultado.setText(R.string.respuesta8);
-                               break;
-                           case 9:
-                               resultado.setText(R.string.respuesta9);
-                               break;
-                           case 10:
-                               resultado.setText(R.string.respuesta10);
-                               break;
-                           case 11:
-                               resultado.setText(R.string.respuesta11);
-                               break;
-                           case 12:
-                               resultado.setText(R.string.respuesta12);
-                               break;
-                           case 13:
-                               resultado.setText(R.string.respuesta13);
-                               break;
-                           case 14:
-                               resultado.setText(R.string.respuesta14);
-                               break;
-
-                       }
-
-
-
-                        respondiendo = false;
+                       setTextResponse(numero);
+                       respondiendo = false;
                     }
                 }.start();
 
@@ -307,7 +181,130 @@ public class JugarActivity extends AppCompatActivity implements SensorEventListe
             // La precisión del sensor ha cambiado
         }
 
+        public void voiceToText(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.start_speech_prompt));
+
+                try{
+                    startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+                }catch(ActivityNotFoundException e){
+
+
+                }
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode){
+
+            case REQ_CODE_SPEECH_INPUT:
+                    if(data != null && resultCode == RESULT_OK){
+                        ArrayList<String> textoObtenido = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        //Log.println(Log.INFO, "lectura de voz", textoObtenido.get(0));
+                       // Log.i("lectura de voz", textoObtenido.get(0));
+                        clickBallResponse(textoObtenido.get(0));
+                    }
+
+                break;
+        }
 
     }
+
+    private void clickBallResponse(final String question){
+
+        if(respondiendo==false) {
+            resultado.setText("");
+            respondiendo = true;
+
+            if (MainActivity.sonido) {
+                mp.start();
+            }
+
+            new CountDownTimer(tiempo_respuesta, 1000) {
+
+
+                @Override
+                public void onTick(long l) {
+                    // do nothing
+                }
+
+                @Override
+                public void onFinish() {
+                    int numero = checkQuestion(question) != 0? checkQuestion(question) : (int) (Math.random() * 14) + 1;
+
+                    setTextResponse(numero);
+                    respondiendo = false;
+                }
+            }.start();
+
+        }
+
+    }
+
+    private void setTextResponse(int Answer){
+
+        switch (Answer){
+
+            case 1:
+                resultado.setText(R.string.respuesta1);
+                break;
+            case 2:
+                resultado.setText(R.string.respuesta2);
+                break;
+            case 3:
+                resultado.setText(R.string.respuesta3);
+                break;
+            case 4:
+                resultado.setText(R.string.respuesta4);
+                break;
+            case 5:
+                resultado.setText(R.string.respuesta5);
+                break;
+            case 6:
+                resultado.setText(R.string.respuesta6);
+                break;
+            case 7:
+                resultado.setText(R.string.respuesta7);
+                break;
+            case 8:
+                resultado.setText(R.string.respuesta8);
+                break;
+            case 9:
+                resultado.setText(R.string.respuesta9);
+                break;
+            case 10:
+                resultado.setText(R.string.respuesta10);
+                break;
+            case 11:
+                resultado.setText(R.string.respuesta11);
+                break;
+            case 12:
+                resultado.setText(R.string.respuesta12);
+                break;
+            case 13:
+                resultado.setText(R.string.respuesta13);
+                break;
+            case 14:
+                resultado.setText(R.string.respuesta14);
+                break;
+            case 25:
+                resultado.setText(R.string.respuesta25);
+                break;
+        }
+
+    }
+
+    private int checkQuestion(String question){
+
+        if(question.toLowerCase(Locale.ROOT).contains(getString(R.string.dumb_question1).toLowerCase(Locale.ROOT))){
+            return 25;
+        }
+        return 0;
+    }
+}
 
 
